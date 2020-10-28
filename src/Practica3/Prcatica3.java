ee0b75b76f4bbd7187b151cee02aa2730702131c
marcos rodriguez <peter99@example.com>
2020-10-28 22:46:42 +0100

    añadimos algun comentario

--- src/Practica3/Prcatica3.java
+++ src/Practica3/Prcatica3.java
@@ -12,6 +12,7 @@ import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
 public class Prcatica3 {
+	//buscamos el xml
 	public static final String DOCUMENTO_XML = "C:\\Users\\mrmar\\OneDrive\\Escritorio\\Universidad\\1ºCuatri\\Desarrollo E Integración De Software\\EjemploStAX-main\\Práctica\\empleados.xml";
     public static final String SALARIO = "salario";
     public static final String NOMBRE = "nombre";
@@ -28,17 +29,20 @@ public class Prcatica3 {
         try {
             inputStream = new FileInputStream(DOCUMENTO_XML);
             xmlStreamReader = xmlInputFactory.createXMLStreamReader(inputStream);
-
+            //recorremos el xml 
             while (xmlStreamReader.hasNext()) {
                 xmlStreamReader.next();
                 
                 if(xmlStreamReader.isStartElement() && NOMBRE.equals(xmlStreamReader.getLocalName())) {
+                	//guardamos el nombre y despues comprobaremos si su salario es mayor de 30000
                    nombreTemp=xmlStreamReader.getElementText();
 
                 }
                 if (xmlStreamReader.isStartElement() && SALARIO.equals(xmlStreamReader.getLocalName())) {
                     String salarioTemp=xmlStreamReader.getElementText();
+                    //buscamos lo que tiene un salio mayor o igual a 30000
                     if (Integer.parseInt(salarioTemp)>=30000) {
+                    	//lo añadimos a la lista
                     	resul.add(nombreTemp);
                     }
                     
@@ -59,6 +63,7 @@ public class Prcatica3 {
 	}
 	public static void main(String[] args) {	
         try {
+        	//imprimimos el resultado
         	System.out.println("Empleados con salario mayor a 30000: "+ numeroDePlazas());
         } catch (IOException e) {
             e.printStackTrace();
